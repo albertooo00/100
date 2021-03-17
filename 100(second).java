43cc6cfc3a5cb81a37d3779a2b2ef64fd98fb5f3
Albertooo Safin <sultanofsicness@icloud.com>
2021-03-17 14:33:13 +0300

    1002103

--- 100(second).java
+++ 100(second).java
@@ -1,12 +1,9 @@
 public class Main {
     public static void main(String[] args) {
         int percent = 100; //процент
-        long score = 1000; //счет
+        long score = 1100; //счет
         long bonus = score / percent; //счет : процент = бонус
-        long limit = 1100;  //минимальная сумма для бонусов
-        if(score < limit) {   //если счет меньше 1100 то бонус = 0
-            bonus = 0;
-        }
         System.out.println(bonus);
+        // если без лимита, то любое значение теперь делится на 100. Хоть 100 хоть 1100. что является не правильным по условию задачи! 
     }
 }
